a85116f4ce533dc66ed2ccc8a8638733109b7048
RebootVanChild <tianchengliu.com@gmail.com>
2023-09-11 00:44:36 +0800

    widget

--- src/napari_load_img_affine/_widget.py
+++ src/napari_load_img_affine/_widget.py
@@ -7,14 +7,31 @@ see: https://napari.org/stable/plugins/guides.html?#widgets
 Replace code below according to your needs.
 """
 from typing import TYPE_CHECKING
-
 from magicgui import magic_factory
-from qtpy.QtWidgets import QHBoxLayout, QPushButton, QWidget
 
 if TYPE_CHECKING:
     import napari
 
 
-@magic_factory
+@magic_factory(
+    call_button="register",
+    img_file={
+        "label": "image",
+        "filter": "*.czi",
+        "tooltip": "Select the image",
+    },
+    transformation_file_type={
+        "choices": [
+            "landmark pairs",
+            "affine matrix",
+        ],
+        "tooltip": "Specify the transformation file type",
+    },
+    transformation_file={
+        "label": "transformation file",
+        "filter": "*.csv",
+        "tooltip": "Select the transformation file",
+    },
+)
 def widget(img_layer: "napari.layers.Image"):
     print(f"you have selected {img_layer}")
