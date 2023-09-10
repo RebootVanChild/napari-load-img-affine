ebe2d497616a64e32e586048efd40e18c36f2022
RebootVanChild <tianchengliu.com@gmail.com>
2023-09-11 01:54:50 +0800

    widget

--- src/napari_load_img_affine/_widget.py
+++ src/napari_load_img_affine/_widget.py
@@ -1,37 +1,36 @@
-"""
-This module is an example of a barebones QWidget plugin for napari
-
-It implements the Widget specification.
-see: https://napari.org/stable/plugins/guides.html?#widgets
-
-Replace code below according to your needs.
-"""
-from typing import TYPE_CHECKING
+from pathlib import Path
+from typing import TYPE_CHECKING, Sequence
 from magicgui import magic_factory
+from napari import Viewer
 
 if TYPE_CHECKING:
     import napari
 
 
 @magic_factory(
-    # call_button="register",
-    # img_file={
-    #     "label": "image",
-    #     "filter": "*.czi",
-    #     "tooltip": "Select the image",
-    # },
-    # transformation_file_type={
-    #     "choices": [
-    #         "landmark pairs",
-    #         "affine matrix",
-    #     ],
-    #     "tooltip": "Specify the transformation file type",
-    # },
-    # transformation_file={
-    #     "label": "transformation file",
-    #     "filter": "*.csv",
-    #     "tooltip": "Select the transformation file",
-    # },
+    call_button="register",
+    img_file={
+        "label": "image",
+        "filter": "*.czi",
+        "tooltip": "Select the image",
+    },
+    transformation_file_type={
+        "choices": [
+            "landmark pairs",
+            "affine matrix",
+        ],
+        "tooltip": "Specify the transformation file type",
+    },
+    transformation_file={
+        "label": "transformation file",
+        "filter": "*.csv",
+        "tooltip": "Select the transformation file",
+    },
 )
-def example_magic_widget(img_layer: "napari.layers.Image"):
-    print(f"you have selected {img_layer}")
+def widget(
+    viewer: Viewer,
+    img_file: Sequence[Path],
+    transformation_file: Sequence[Path],
+    transformation_file_type: str = "landmark pairs",
+):
+    print(f"you have selected")
